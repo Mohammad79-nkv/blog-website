f5e2894f4c1822648425b7b4f2fd444b958c654a
mohammad <mohammadnv79@gmail.com>
2022-04-17 08:34:24 +0430

    fixed image in React Markdown

--- components/post/postDetail/PostContent.jsx
+++ components/post/postDetail/PostContent.jsx
@@ -1,14 +1,22 @@
-import PostHeader from './PostHeader';
-import ReactMarkdown from 'react-markdown';
-import classes from './PostContent.module.css'
+import Image from "next/image";
+import PostHeader from "./PostHeader";
+import ReactMarkdown from "react-markdown";
+import classes from "./PostContent.module.css";
 
-const PostContent = ({post}) => {
-    return ( 
-        <article className={classes.content}>
-            <PostHeader {...post}/>
-            <ReactMarkdown>{post.content}</ReactMarkdown>
-        </article>
-     );
-}
- 
+const PostContent = ({ post }) => {
+
+  function image({ src, alt }) {
+    return <Image src={src} alt={alt} width={600} height={300} />;
+  }
+
+  return (
+    <article className={classes.content}>
+      <PostHeader {...post} />
+      <ReactMarkdown components={{ img: image }}>
+        {post.content}
+      </ReactMarkdown>
+    </article>
+  );
+};
+
 export default PostContent;
